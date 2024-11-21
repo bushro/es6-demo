ec65931728d723100960681de08d780473459d2a
luo.qiang <1137824185@qq.com>
2024-11-21 09:49:22 +0800

    引入其它js使用

---
+++ src/es6/chapter5-4.1.js
@@ -0,0 +1,8 @@
+import test from './chapter5-4.2.js'
+// import * as test from './chapter5-4.2.js'
+
+// console.log(a, sayHello, test)
+// console.log(test)
+// console.log(a);
+
+console.log(test.a)
